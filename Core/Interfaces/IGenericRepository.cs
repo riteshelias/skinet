a8470972da1d7c2f610609c9589cc6fcdab2bcd1
riteshelias <ritesh.elias@gmail.com>
2021-03-29 11:53:57 +0530

    End of Section 6

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -8,8 +8,13 @@ namespace Core.Interfaces
     public interface IGenericRepository<T> where T : BaseEntity
     {
         Task<T> GetByIdAsync(int id);
+
         Task<IReadOnlyList<T>> ListAllAsync(); 
+
         Task<T> GetEntitywithSpec(ISpecification<T> spec);
+
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+
+        Task<int> CountAsync(ISpecification<T> spec);
     }
 }
--- Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
@@ -6,10 +6,33 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandSpecification()
+        public ProductsWithTypesAndBrandSpecification(ProductSpecParams prodParams) : 
+            base( x =>
+                    (string.IsNullOrEmpty(prodParams.Search) || x.Name.ToLower().Contains(prodParams.Search)) &&
+                    (!prodParams.BrandId.HasValue || x.ProductBrandId == prodParams.BrandId) &&
+                    (!prodParams.TypeId.HasValue || x.ProductTypeId == prodParams.TypeId)
+                )
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(prodParams.PageSize * (prodParams.PageIndex - 1), prodParams.PageSize);
+
+            if (!string.IsNullOrEmpty(prodParams.Sort))
+            {
+                switch (prodParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandSpecification(int id) : base(x => x.Id == id)
